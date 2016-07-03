ba3fe0535f6147a5f8ad8fbf6ab861f4005dcfc4
mhmmdd <mhmmd.dd@gmail.com>
2016-07-03 11:54:31 +0300

    Fixes #1

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,10 +1,12 @@
 import {Component} from 'angular2/core';
+import {TemplateDrivenFormComponent} from "./template-driven-form";
 
 @Component({
   selector: 'my-app',
   template: `
-<h1>hello</h1>
-  `
+     <my-template-driven></my-template-driven>
+  `,
+  directives: [TemplateDrivenFormComponent]
 })
 
 export class AppComponent {
